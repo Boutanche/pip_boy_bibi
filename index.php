0d94c6e96a44206f75e574212878c335c18af9e2
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-04-10 01:52:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Fallout 4 Pip-Boy BiBi CV</title>
+        <!--INSERT HERE : link : Favicon + CSS -->
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <p>Bientôt ici CV de Benoit BERTRAND</p>
+
+    </body>
+</html>
+
+
+
+<?php
