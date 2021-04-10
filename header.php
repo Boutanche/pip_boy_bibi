5794e9f6c638563d7aa3c1198723cda928e3c171
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-04-11 01:37:06 +0200

    header menu

---
+++ header.php
@@ -0,0 +1,15 @@
+<h1>CV BENOIT BERTRAND</h1>
+<div class="header">
+    <ul class="tabs">
+        <li class="tab-active"><a href="#stat-status-section">STAT</a></li>
+        <li><a href="#exp-jobs-section">EXP</a></li>
+        <li><a href="#data-section">DATA</a></li>
+        <li><a href="#map-section">MAP</a></li>
+        <li><a href="#special-section">S.P.E.C.I.A.L</a></li>
+    </ul>
+</div>
+<div class="header-lines">
+    <img id="header-line-before" src="img/header_line_before.png" alt="graphical stuff">
+    <img id="header-line-after" src="img/header_line_after.png" alt="graphical stuff">
+</div>
+
--- index.php
+++ index.php
@@ -6,12 +6,26 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Fallout 4 Pip-Boy BiBi CV</title>
-        <!--INSERT HERE : link : Favicon + CSS -->
+        <!--INSERT HERE : Favicon -->
         <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet">
     </head>
     <body>
-        <p>Bientôt ici CV de Benoit BERTRAND</p>
+    <div class="load">
+        <pre class="term">cda3@bert:~$</pre>
+    </div>
+    <header>
+        <?php include("header.php");?>
+    </header>
 
+    <!-- Console_Loading-->
+    <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous">
+    </script>
+    <script src="./js/console_loading.js"></script>
+    <script src="js/tabs.js"></script>
     </body>
 </html>
 
